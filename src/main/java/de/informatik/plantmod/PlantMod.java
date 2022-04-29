fdddb6fd10bf3f66d8f2078c44b995146f0510a5
FU3000 <frederik.umlauf@igs-buchholz.de>
2022-04-29 14:07:40 +0200

    Initial commit

---
+++ src/main/java/de/informatik/plantmod/PlantMod.java
@@ -0,0 +1,35 @@
+package de.informatik.plantmod;
+
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod( PlantMod.MOD_ID)
+public class PlantMod {
+    public static final String MOD_ID = "plantmod";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PlantMod()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
